958ddf4411572d71ca8209f712abe640fb5bca7e
Semen <skorohodov-96@yandex.ru>
2024-02-11 23:07:51 +0300

    Fix: codestyle

--- src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/BookingServiceImpl.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.booking.dto.BookingDto;

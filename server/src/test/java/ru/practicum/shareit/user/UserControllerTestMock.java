21e6bbda302e0d8a76679529bc3e4c6ee04f0f21
Semen <skorohodov-96@yandex.ru>
2024-03-10 21:57:43 +0300

    Try add test

---
+++ server/src/test/java/ru/practicum/shareit/user/UserControllerTestMock.java
@@ -0,0 +1,102 @@
+package ru.practicum.shareit.user;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.practicum.shareit.exception.ConflictException;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+public class UserControllerTestMock {
+    @Mock
+    private UserServiceImpl userService;
+    @InjectMocks
+    private UserController userController;
+    UserDto userDto1 = new UserDto();
+    UserDto userDto2 = new UserDto();
+    UserDto userDto3 = new UserDto();
+    UserDto userDto4 = new UserDto();
+    UserDto userDto5 = new UserDto();
+    List<UserDto> userDtos = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        userDto1.setName("test");
+        userDto1.setEmail("test@test.ru");
+        userDto2.setName("test2");
+        userDto3.setName("test");
+        userDto3.setEmail("test@test.ru");
+        userDto4.setEmail("test4@test4.ru");
+        userDto5.setId(1);
+        userDto5.setEmail("test5@test5.ru");
+        userDto5.setName("test5");
+        userDtos.add(userDto5);
+    }
+
+    @Test
+    void createUser() throws Exception {
+        Mockito.when(userService.createUser(Mockito.any())).thenReturn(userDto1);
+        UserDto userDtoCreate = userController.createUser(userDto1);
+        assertThat(userDtoCreate.getId(), equalTo(userDto1.getId()));
+        assertThat(userDtoCreate.getName(), equalTo(userDto1.getName()));
+        assertThat(userDtoCreate.getEmail(), equalTo(userDto1.getEmail()));
+    }
+
+
+    @Test
+    void createUserWithExistEmail() throws Exception {
+        Mockito.when(userService.createUser(Mockito.any()))
+                .thenThrow(new ConflictException("Такой email уже существует"));
+        ConflictException throwable = assertThrows(ConflictException.class,
+                () -> userController.createUser(userDto3));
+        assertThat(throwable.getMessage(), equalTo("Такой email уже существует"));
+    }
+
+    @Test
+    void getAllUser() throws Exception {
+        Mockito.when(userService.getAllUser()).thenReturn(userDtos);
+        List<UserDto> userDtosGet = userController.getAllUser();
+        assertThat(userDtosGet.size(), equalTo(userDtos.size()));
+    }
+
+    @Test
+    void findUserById() throws Exception {
+        Mockito.when(userService.findUserById(Mockito.anyLong())).thenReturn(userDto5);
+        UserDto userDtoGet = userController.findUserById(1);
+        assertThat(userDtoGet.getId(), equalTo(userDto5.getId()));
+    }
+
+    @Test
+    void findUserByIdWrongId() throws Exception {
+        Mockito.when(userService.findUserById(Mockito.anyLong()))
+                .thenThrow(new NotFoundException("Пользователь не найден"));
+        NotFoundException notFoundException = assertThrows(NotFoundException.class,
+                () -> userController.findUserById(1));
+        assertThat(notFoundException.getMessage(), equalTo("Пользователь не найден"));
+    }
+
+    @Test
+    void updateUser() throws Exception {
+        Mockito.when(userService.updateUser(Mockito.anyLong(), Mockito.any())).thenReturn(userDto5);
+        UserDto userDtoGet = userController.updateUser(1, userDto4);
+        assertThat(userDtoGet.getId(), equalTo(userDto5.getId()));
+    }
+
+    @Test
+    void deleteUser() throws Exception {
+        userController.deleteUserById(1);
+        Mockito.verify(userService).deleteUserById(1);
+    }
+}
---
+++ server/src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
@@ -0,0 +1,90 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class UserServiceImplTest {
+
+
+    private final EntityManager em;
+    private final UserService userService;
+    UserDto userDto1 = new UserDto();
+    UserDto userDto2 = new UserDto();
+    UserDto userDto3 = new UserDto();
+    UserDto userDto4 = new UserDto();
+
+    @BeforeEach
+    void setUp() {
+        userDto1.setName("test");
+        userDto1.setEmail("test@test.ru");
+        userDto2.setEmail("update@update.ru");
+        userDto3.setName("test3");
+        userDto4.setName("test4");
+        userDto4.setEmail("test4@test4.ru");
+    }
+
+    @Test
+    void createUserAndGetAllUser() {
+        userService.createUser(userDto1);
+        TypedQuery<User> query = em.createQuery("SELECT u FROM User u WHERE u.email = :email", User.class);
+        User user = query.setParameter("email", userDto1.getEmail()).getSingleResult();
+        assertThat(user.getId(), notNullValue());
+        assertThat(user.getName(), equalTo(userDto1.getName()));
+        assertThat(user.getEmail(), equalTo(userDto1.getEmail()));
+        List<UserDto> userDtos = userService.getAllUser();
+        TypedQuery<User> query1 = em.createQuery("SELECT u FROM User u", User.class);
+        List<User> users = query1.getResultList();
+        assertThat(users.size(), equalTo(userDtos.size()));
+        assertThat(users.get(0).getId(), equalTo(userDtos.get(0).getId()));
+    }
+
+    @Test
+    void updateUserAndDeleteUser() {
+        UserDto userDto = userService.createUser(userDto1);
+        userService.updateUser(1, userDto2);
+        TypedQuery<User> query = em.createQuery("SELECT u FROM User u WHERE u.id = :id", User.class);
+        User user = query.setParameter("id", userDto.getId()).getSingleResult();
+        assertThat(user.getId(), notNullValue());
+        assertThat(user.getEmail(), equalTo(userDto2.getEmail()));
+        UserDto userDto5 = userService.updateUser(userDto.getId(), userDto3);
+        assertThat(userDto5.getName(), equalTo(userDto3.getName()));
+        UserDto userDto6 = userService.updateUser(userDto.getId(), userDto4);
+        assertThat(userDto6.getName(), equalTo(userDto4.getName()));
+        userService.deleteUserById(userDto.getId());
+        assertThat(userService.getAllUser().size(), equalTo(0));
+    }
+
+    @Test
+    void findUserById() {
+        userService.createUser(userDto1);
+        UserDto userDto = userService.findUserById(1);
+        TypedQuery<User> query = em.createQuery("SELECT u FROM User u WHERE u.email = :email", User.class);
+        User user = query.setParameter("email", userDto1.getEmail()).getSingleResult();
+        assertThat(user.getEmail(), equalTo(userDto.getEmail()));
+        assertThat(user.getId(), equalTo(userDto.getId()));
+        try {
+            userService.findUserById(100);
+        } catch (NotFoundException e) {
+            assertThat(e.getMessage(), equalTo("Пользователь не найден"));
+        }
+    }
+}
